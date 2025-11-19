444dc02a0aef41915eb0816bf6cec6d160abfa19
Manju-dev12 <heyitsjuanm@gmail.com>
2025-11-19 09:34:52 -0600

    Update MyVulnerableApp.java

--- src/main/java/MyVulnerableApp.java
+++ src/main/java/MyVulnerableApp.java
@@ -2,21 +2,22 @@ package src.main.java;
 
 import java.sql.Connection;
 import java.sql.Statement;
-import java.util.Scanner;
+import javax.servlet.http.HttpServletRequest; // Se necesita para simular una app web
 
 public class MyVulnerableApp {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter username:");
-        String userInput = scanner.nextLine(); // Fuente de datos contaminados (Source)
+    // Método simulado que recibe una petición HTTP
+    public void processRequest(HttpServletRequest request) { 
+
+        // FUENTE (Source): CodeQL siempre detecta los parámetros HTTP como datos contaminados.
+        String userInput = request.getParameter("username"); 
 
         // Construcción vulnerable de la consulta: SQL Injection
         String query = "SELECT * FROM users WHERE username = '" + userInput + "';"; 
 
         try {
-            Connection c = null; // Simula la conexión
+            Connection c = null; 
             Statement s = c.createStatement();
-            s.executeQuery(query); // Sumidero (Sink)
+            s.executeQuery(query); // SUMIDERO (Sink)
 
         } catch (Exception e) {
             // Manejo de errores
